bb8695ff7473a633d341bfa14b4e9e4128a33878
phoenix-Force <paradox.joe.007@gmail.com>
2019-10-22 18:13:29 +0530

    comment-box

---
+++ src/components/timeline/postContainer/commentBox.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-row style="height:100%;-webkit-box-shadow: 2px 0px 14px 2px rgba(0,0,0,0.88);box-shadow: 2px 0px 14px 2px rgba(0,0,0,0.88);border-radius: 10px;">
+    <el-col :span= "4">
+      <el-button v-if ="liked" class="btnico" @click ="fk()"><i class="fa fa-thumbs-up" style="color: mediumpurple;"></i></el-button>
+      <el-button v-else @click ="fk()" class="btnico"><i  class="fa fa-thumbs-o-up" style="color: mediumpurple;"></i></el-button>
+    </el-col>
+    <el-col :span= "3">
+      <p>{{totalLiked}}</p>
+    </el-col>
+    <el-col :span= "4">
+      <el-button v-if ="dislike" class="btnico" @click ="dk()"><i  class="fa fa-thumbs-down" style="color: mediumpurple;"></i></el-button>
+      <el-button v-else  class="btnico" @click ="dk()"><i  class="fa fa-thumbs-o-down" style="color: mediumpurple;"></i></el-button>
+    </el-col>
+    <el-col :span= "3">
+      <p>{{totalDislike}}</p>
+      </el-col>
+    <el-col :span= "4">
+      <el-button v-if ="comment"   class="btnico"><i  class="fa fa-comment" style="color: mediumpurple;"></i></el-button>
+      <el-button v-else class="btnico"><i  class="fa fa-comment-o" style="color: mediumpurple;"></i></el-button>
+    </el-col>
+    <el-col :span= "3" >
+      <p>12</p>
+    </el-col>
+  </el-row>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.btnico{
+  font-size: 20px;
+  height:50px;
+  width:50px;
+  border:none;
+  background-color: Transparent;
+  background-repeat:no-repeat;
+  border: none;
+  cursor:pointer;
+  margin-left:1%;
+}
+.btnico:hover{
+  font-size: 35px;
+  animation: heartBeat 2s infinite;
+}
+.dislike{
+  background-color:brown;
+}
+.fa-thumbs-o-up:before {
+    content: "\f087";
+
+}
+.btnico:active{
+  animation: 	swing 2s infinite;
+}
+@media screen and (max-width: 1240px) {
+  .mdq{
+    margin-left:10%;
+  }
+}
+</style>
--- src/components/timeline/postContainer/postContainer.vue
+++ src/components/timeline/postContainer/postContainer.vue
@@ -1,28 +1,28 @@
 <template>
 <el-row style="height:30vh">
-  <el-row style="heigth:10%;">
-    <el-col :span = "10">
-      <el-avatar shape="square" :size="100" :fit="fit" :src ="avatar"></el-avatar>
+  <el-row style="heigth:10%;-webkit-box-shadow: 2px 0px 14px 2px rgba(0,0,0,0.88);box-shadow: 2px 0px 14px 2px rgba(0,0,0,0.88);border-radius: 25px;">
+    <el-col :span="3">
+      <el-avatar shape="circle" :size="100"  :src ="avatar"></el-avatar>
+    </el-col>
+    <el-col :span="20">
+      <el-row style="height:30%;padding-top:2px;top:0;">
+      <p class="mdq">Paradox</p>
+      </el-row>
+      <el-row style="height:30%;">
+        <p  class="mdq">Uploaded at:</p>
+      </el-row>
     </el-col>
   </el-row>
-  <el-row style="heigth:80%;">
+  <el-row style="height:10%">
+
+  </el-row>
+  <el-row style="heigth:70%;-webkit-box-shadow: 2px 0px 14px 2px rgba(0,0,0,0.88);box-shadow: 2px 0px 14px 2px rgba(0,0,0,0.88);border-radius: 25px;">
     <el-col>
-      <el-image :src = "img_url" style="height:100%;width:100%"></el-image>
+      <el-image :src = "img_url" style="height:95%;width:99%;margin-top:3px;margin-left:5px;overflow:hidden;border-radius:15px;"></el-image>
     </el-col>
   </el-row>
   <el-row>
-    <el-col>
-
-    </el-col>
-    <el-col>
-
-    </el-col>
-    <el-col>
-
-    </el-col>
-    <el-col>
-
-    </el-col>
+    <comment-box></comment-box>
   </el-row>
   <el-row style="heigth:10%;">
   </el-row>
@@ -30,13 +30,20 @@
 
 </template>
 <script>
+import cmntbx from "./commentBox.vue"
 export default {
   data(){
     return {
       img_url:"../../../../src/assets/s2.jpg",
-      avatar:"https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+      avatar:"https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      totalLiked:20,
+      totalDislike:25,
     }
+  },
+  components:{
+    commentBox:cmntbx
   }
+
 }
 </script>
 <style lang="scss" scoped>
